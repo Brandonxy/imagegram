5a8b965c36e2c2a0b9a1ae8f31a2a437ecc555de
Brandon <>
2019-04-27 16:16:58 -0400

    Fix required parameter in route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
-                                <a href="{{ route('profile') }}">Perfil</a>
+                                <a href="{{ route('profile', ['id' => Auth::user()->id]) }}">Perfil</a>
                             </li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
